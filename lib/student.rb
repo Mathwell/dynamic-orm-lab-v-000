40dd1b0e852325cb3125f053e55bfc91cba46f3e
Olena Ageyeva <eageeva@hotmail.com>
2017-12-04 00:45:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,8 @@ require 'interactive_record.rb'
 class Student < InteractiveRecord
 
   def initialize
+    options.each do |property, value|
+    self.send("#{property}=", value)
   end
 
   def self.table_name
