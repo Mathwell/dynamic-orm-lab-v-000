d53862cb8e3205bfe093b487241178a1f18e8554
Olena Ageyeva <eageeva@hotmail.com>
2017-12-04 00:40:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,37 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+  def initialize
+  end
+
+  def table_name
+     self.to_s.downcase.pluralize
+  end
+
+  def column_names
+  end
+
+  def attr_accessor
+  end
+
+  def table_name_for_insert
+  end
+
+  def col_names_for_insert
+  end
+
+  def values_for_insert
+  end
+
+  def save
+  end
+
+  def find_by_name
+  end
+
+  def find_by
+  end
+
+  end
+
 end
