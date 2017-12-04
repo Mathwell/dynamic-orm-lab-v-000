f3b98b3824a5fb0f4467c4ab15c8d5ad904a4b5d
Olena Ageyeva <eageeva@hotmail.com>
2017-12-04 00:50:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,6 +42,11 @@ class Student < InteractiveRecord
   end
 
   def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
   end
 
   def save
